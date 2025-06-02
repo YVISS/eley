31a31f58053534b6511c79e1b82428b32f9de6b2
lewee aaron ombac <laombac14@gmail.com>
2025-06-02 10:30:07 +0800

    0602 1030

--- eleypfolio/src/main.jsx
+++ eleypfolio/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import './css/divider.css'
 import './css/header.css'
+import './css/reset.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
