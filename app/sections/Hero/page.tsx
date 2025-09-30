0e3bb50d0b9f6200dc04042b86c475e5cebe6c71
Ombac LA <laombac14@gmail.com>
2025-09-30 16:52:16 +0800

    added skills section

--- app/sections/Hero/page.tsx
+++ app/sections/Hero/page.tsx
@@ -8,8 +8,8 @@ export default function Hero(){
             <h1 className="w-[60%] text-center tracking-tight">Frontend developer crafting digital experiences</h1>
             <p className="w-[70%] text-center text__body">I build clean, responsive websites that solve real problems. My code is precise, my designs are sharp.</p>
             <div className={`${headings.className} hero__btns gap-4 flex flex-row align-center justify-center mt-8` }>
-                <button className="rounded-lg px-4 py-[1vw] text-white bg-black">Explore</button>
-                <button className="rounded-lg px-5 py-[1vw] border-1">Hire</button>
+                <button className="rounded-lg px-4 py-2 text-white bg-black">Explore</button>
+                <button className="rounded-lg px-5 py-2 border-1 ">Hire</button>
             </div>
         </section>
     );
--- app/sections/Skills/page.tsx
+++ app/sections/Skills/page.tsx
@@ -1,9 +1,17 @@
-
+import { headings } from "../../fonts/fonts";
+import { body } from "../../fonts/fonts";
 
 export default function Skills() {
     return (
-       <section>
-        <h1>Skills</h1>
-       </section>
+        <section className="grid grid-cols-2">
+            <div className="skills__info text-start">
+                <p className={`${body.className} antialiazed`}>Skills</p>
+                <h1 className={`${headings.className} antialiazed`}>Technical skills that transform digital landscapes</h1>
+                <p>Passionate about creating intuitive and efficient web solutions with modern technologies.</p>
+            </div>
+            <div className="skills__imgae">
+
+            </div>
+        </section>
     );
 }
